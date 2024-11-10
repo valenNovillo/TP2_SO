75f5c5c799002b3b9f12bbcad73b2a1fd5740c60
valenNovillo <VANOVILLO@ITBA.EDU.AR>
2024-11-10 20:42:28 -0300

    update

--- x64barebones/Userland/SampleCodeModule/Library/philosophers.c
+++ x64barebones/Userland/SampleCodeModule/Library/philosophers.c
@@ -7,7 +7,7 @@
 #define MAX_PHILOS 10
 #define THINKING 0
 #define EATING 1
-#define THINKING_TIME 2000
+#define THINKING_TIME 10000
 
 static char *philos[MAX_PHILOS] = {
     "philo00", "philo01", "philo02", "philo03",
@@ -16,7 +16,6 @@ static char *philos[MAX_PHILOS] = {
 };
 
 static sem_ptr forks[MAX_PHILOS] = {NULL};
-
 static sem_ptr modifing;
 
 int philos_pids[MAX_PHILOS];
@@ -27,16 +26,27 @@ int16_t philo_fds[3] = {NO_INPUT, STDOUT, STDERR};
 int init_philos_restaurant(uint64_t argc, char *argv[]) {
     int cant_philos = MIN_PHILOS;
     modifing = my_sem_create(MAX_PHILOS, 1);
+    if(modifing == NULL) {
+        printErr("\nError creating new semaphore\n"); 
+        return -1;
+    }
+
     for (int j = 0; j < MIN_PHILOS; j++) {     
         philos_states[j] = THINKING;
     }
 
     char* args[3];
+    char id[3];
     for(int i=0; i < MIN_PHILOS; i++) {
         args[0] = philos[i];
-        intToString(i, args[1]);    
+        intToString(i, id);    
+        args[1] = id;
         args[2] = 0;
         forks[i] = my_sem_create(i, 1);
+        if(forks[i] == NULL) {
+            printErr("\nError creating new semaphore\n"); 
+            return -1;
+        }
         philos_pids[i] = create_process((Main)philosopher, args, philos[i] , 2, philo_fds);
     }
 
@@ -46,29 +56,29 @@ int init_philos_restaurant(uint64_t argc, char *argv[]) {
 int philosopher(uint64_t argc, char *argv[]) {
     int philo_id = stringToInt(argv[1]);
 
-    int left_fork_id = philo_id; 
-    int right_fork_id = (philo_id + 1) % MIN_PHILOS; 
+    // int left_fork_id = philo_id; 
+    // int right_fork_id = (philo_id + 1) % MIN_PHILOS; 
 
-    sem_ptr* left_fork_sem;
-    sem_ptr* right_fork_sem;
+    // sem_ptr* left_fork_sem;
+    // sem_ptr* right_fork_sem;
 
-    if((left_fork_sem = my_sem_create(left_fork_id, 1)) == NULL || (right_fork_sem = my_sem_create(right_fork_id, 1)) == NULL) {
-        printErr("\nError creating new semaphore\n");   //TO-DO: se rompe aca!!!!!!!
-        return -1;
-    }
+    // if((left_fork_sem = my_sem_create(left_fork_id, 1)) == NULL || (right_fork_sem = my_sem_create(right_fork_id, 1)) == NULL) {
+    //     printErr("\nError creating new semaphore\n");   //TO-DO: se rompe aca!!!!!!!
+    //     return -1;
+    // }
 
     while(1) {
         if(philo_id % 2) {
-            my_sem_wait(left_fork_sem);
-            my_sem_wait(right_fork_sem);
+            my_sem_wait(forks[philo_id]);
+            my_sem_wait(forks[(philo_id + 1) % MIN_PHILOS]);
         } else {
-            my_sem_wait(right_fork_sem);
-            my_sem_wait(left_fork_sem);
+            my_sem_wait(forks[(philo_id + 1) % MIN_PHILOS]);
+            my_sem_wait(forks[philo_id]);
         }
         philos_states[philo_id] = EATING;
         eat(philo_id);
-        my_sem_post(left_fork_sem);
-        my_sem_post(right_fork_sem);
+        my_sem_post(forks[philo_id]);
+        my_sem_post(forks[(philo_id + 1) % MIN_PHILOS]);
         philos_states[philo_id] = THINKING;
         sleep(THINKING_TIME);
     }
